9e6e74ab1238592b893293e6ce37eba40815d334
Sultan Gillani <sultan@veracity.io>
2019-10-06 18:51:13 -0400

    complete

--- .babelrc.js
+++ .babelrc.js
@@ -1,38 +1,38 @@
 module.exports = {
-"env": {
-  "development": {
-    "presets": [
-      "@babel/preset-env",
-      "@babel/preset-react"
-    ],
-      "plugins": [
-      "babel-plugin-styled-components",
-      "@babel/plugin-transform-runtime",
-      "@babel/plugin-proposal-class-properties",
-      "react-hot-loader/babel"
-    ]
-  },
-  "production": {
-    "presets": [
-      "@babel/preset-env",
-      "@babel/preset-react"
-    ],
-      "plugins": [
-      "@babel/plugin-transform-runtime",
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-transform-react-constant-elements",
-      "transform-react-remove-prop-types"
-    ]
-  },
-  "test": {
-    "presets": [
-      "@babel/preset-env",
-      "@babel/preset-react"
-    ],
-      "plugins": [
-      "@babel/plugin-transform-runtime",
-      "@babel/plugin-proposal-class-properties"
-    ]
+  "env": {
+    "development": {
+      "presets": [
+        "@babel/preset-env",
+        "@babel/preset-react"
+      ],
+        "plugins": [
+        "babel-plugin-styled-components",
+        "@babel/plugin-transform-runtime",
+        "@babel/plugin-proposal-class-properties",
+        "react-hot-loader/babel"
+      ]
+    },
+    "production": {
+      "presets": [
+        "@babel/preset-env",
+        "@babel/preset-react"
+      ],
+        "plugins": [
+        "@babel/plugin-transform-runtime",
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-transform-react-constant-elements",
+        "transform-react-remove-prop-types"
+      ]
+    },
+    "test": {
+      "presets": [
+        "@babel/preset-env",
+        "@babel/preset-react"
+      ],
+        "plugins": [
+        "@babel/plugin-transform-runtime",
+        "@babel/plugin-proposal-class-properties"
+      ]
+    }
   }
-}
-}
+};
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  printWidth: 140,
   arrowParens: 'always',
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  tabWidth: 2,
   bracketSpacing: true,
   jsxBracketSameLine: false,
+  printWidth: 140,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
 };
